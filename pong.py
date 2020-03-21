86ef78f1fcca781797b580c00cfc37ab6032b6b1
jysim3 <junyang.0607@gmail.com>
2020-03-21 18:55:44 +1100

    pong game

---
+++ pong.py
@@ -0,0 +1,80 @@
+import math
+UP, STATIONARY, DOWN = -1, 0, 1
+
+
+class Pong():
+    def __init__(self, size, pole_size = (1,10)):
+        width, height = size
+        self.width = width
+        self.height = height
+        self.player1 = self.PongPlayer(10, height//2, pole_size[1], height)
+        self.player2 = self.PongPlayer(width-10,height//2, pole_size[1], height)
+        self.ball = self.Ball(10, height//2)
+
+    def player2_up(self):
+        self.player2.set_direction(UP)
+    def player2_down(self):
+        self.player2.set_direction(DOWN)
+    def player1_up(self):
+        self.player1.set_direction(UP)
+    def player1_down(self):
+        self.player1.set_direction(DOWN)
+
+    def info(self):
+        update = self.update
+        self.update = []
+        return update
+
+    def tick(self):
+        #self.ball.tick()
+        self.ball.check_and_bounce(self.player2.x, self.player2.y,self.player2.height)
+        self.ball.check_and_bounce(self.player1.x, self.player1.y,self.player1.height)
+        ball_move = self.ball.tick(0, self.height)
+        p1_move = self.player1.move()
+        p2_move = self.player2.move()
+        self.update = [ball_move,p1_move,p2_move]
+        return 0 <= self.ball.x < self.width
+
+
+
+    class Ball() :
+        def __init__(self, x, y, v=5):
+            self.x = x
+            self.y = y
+            self.angle = [UP,UP]
+        def check_and_bounce(self,x, y, height):
+            if self.x == x:
+                if 0 <= self.y - y < height//2:
+                    self.angle[0] *= -1
+                    self.angle[1] = UP
+                if height//2 <= self.y - y < height:
+                    self.angle[0] *= -1
+                    self.angle[1] = DOWN
+
+        def tick(self, top, bottom):
+            self.x += self.angle[0]
+            if top >= self.y + self.angle[1] or self.y + self.angle[1] >= bottom:
+                self.angle[1] *= -1
+            self.y += self.angle[1]
+            return self.angle
+        def info(self):
+            return self.x, self.y, 1, 1
+
+    class PongPlayer():
+        def __init__(self, x,y, height, screen_height = 100):
+            self.x = x
+            self.y = y
+            self.screen_height = screen_height
+            self.height = 10
+            self.direction = STATIONARY
+        def set_direction(self, direction):
+            self.direction = direction 
+        def info(self):
+            return self.x, self.y, 1, self.height
+        def move(self):
+            m = STATIONARY
+            if 0 < self.y + self.direction and self.y + self.direction + self.height < self.screen_height:
+                self.y += self.direction
+                m = self.direction
+            self.direction = STATIONARY
+            return (0, m)
---
+++ pongInterface.py
@@ -0,0 +1,81 @@
+import sys, pygame
+import numpy as np
+from time import sleep
+from pong import Pong
+LEFT, RIGHT, UP, DOWN = pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN
+W, A, S, D = pygame.K_w, pygame.K_a, pygame.K_s, pygame.K_d
+QUIT, KEYDOWN = pygame.QUIT, pygame.KEYDOWN
+class Game():
+    def __init__(self, screen_size):
+
+        self.player_color = [255,255,255]
+        self.screen_size = (screen_size)
+        self.game_size = (x//10 for x in self.screen_size)
+        self.speed = .05
+
+        self.inactive = [0,0,0]
+        self.border = 0
+
+        screen = pygame.display.set_mode(self.screen_size)
+        game = Pong(self.game_size)
+        screen.fill([0,0,0])
+
+        x, y, height, width = game.ball.info()
+        ball = pygame.Rect(x*10,y*10,height*10,width*10)
+        screen.fill(self.player_color, ball)
+
+        x, y, height, width = game.player1.info()
+        player1 = pygame.Rect(x*10,y*10,height*10,width*10)
+        screen.fill(self.player_color, player1)
+
+        x, y, height, width = game.player2.info()
+        player2 = pygame.Rect(x*10,y*10,height*10,width*10)
+        screen.fill(self.player_color, player2)
+
+        self.game = game
+        self.ball = ball
+        self.player1 = player1
+        self.player2 = player2
+        self.screen = screen
+    def run(self,  n_life=5, get_action=None, Ai=None):
+        while n_life > 0:
+            pygame.init()
+            actions = pygame.event.get()
+            for event in actions:
+                if event.type == QUIT:
+                    sys.exit()
+            keys = pygame.key.get_pressed()
+
+            if keys[W]:
+                self.game.player2_up()
+            if keys[S]:
+                self.game.player2_down()
+            if keys[UP]:
+                self.game.player1_up()
+            if keys[DOWN]:
+                self.game.player1_down()
+            if not self.game.tick():
+                self.__init__(self.screen_size)
+                n_life -= 1
+                continue
+            ball_update, p1_update, p2_update = self.game.info()
+
+            
+            self.ball.move_ip(*[x*10 for x in ball_update])
+            self.player1.move_ip(*[x*10 for x in p1_update])
+            self.player2.move_ip(*[x*10 for x in p2_update])
+
+            self.screen.fill([0,0,0])
+            pygame.draw.rect(self.screen, (128,128,128), self.player1, 0)
+            pygame.draw.rect(self.screen, (128,128,128), self.ball, 0)
+            pygame.draw.rect(self.screen, (128,128,128), self.player2, 0)
+
+
+            pygame.display.flip()
+
+            sys.stdout.flush()
+            sleep(self.speed)
+
+if __name__=="__main__":
+    g = Game((910,700))
+    g.run()
